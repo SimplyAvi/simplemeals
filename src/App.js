48a9a4acd38e91d526b68722f7a5cfa8c68af66b
SimplyAvi <a.totaram@gmail.com>
2022-01-10 20:05:19 -0500

    added react router dom for navagation, had to downgrade rrd to 5.3.0, to get it working for a simple  route change to /grocery, will need to add other links and pages afterwards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
+
+   
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 
+const GroceryPage = () => (
+  <div>
+    <h1>Grocery PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-        <HomePage/>
+      <Link>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={GroceryPage} />
+      </Link>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,24 +13,28 @@ class Directory extends React.Component {
         {
           title: 'Grocery',
           subtitle: 'Shop Now',
+          linkUrl: 'grocery',
           imageUrl: 'https://images.pexels.com/photos/95425/pexels-photo-95425.jpeg?cs=srgb&dl=pexels-erik-scheel-95425.jpg&fm=jpg',
           id: 1
         },
         {
           title: 'Allergies',
           subtitle: 'Let us know what to avoid',
+          linkUrl: '',
           imageUrl: 'https://images.pexels.com/photos/6097883/pexels-photo-6097883.jpeg?cs=srgb&dl=pexels-laura-james-6097883.jpg&fm=jpg',
           id: 2
         },
         {
           title: 'Deals',
           subtitle: 'Shop Now',
+          linkUrl: '',
           imageUrl: 'https://images.pexels.com/photos/5869604/pexels-photo-5869604.jpeg?cs=srgb&dl=pexels-max-fischer-5869604.jpg&fm=jpg',
           id: 3
         },
         {
           title: 'Recipes',
           subtitle: 'Try Something New',
+          linkUrl: '',
           imageUrl: 'https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?cs=srgb&dl=pexels-dapurmelodi-1109197.jpg&fm=jpg',
           size: 'large',
           id: 4
@@ -38,6 +42,7 @@ class Directory extends React.Component {
         {
           title: 'Restaurant',
           subtitle: 'Shop Now',
+          linkUrl: '',
           imageUrl: 'https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?cs=srgb&dl=pexels-chan-walrus-958545.jpg&fm=jpg',
           size: 'large',
           id: 5
@@ -49,8 +54,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, subtitle, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} subtitle={subtitle} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({  id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
